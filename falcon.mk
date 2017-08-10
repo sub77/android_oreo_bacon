20b1b95fd97a872e54cf3b6d4cfab023edd8ed7f
Adrian DC <radian.dc@gmail.com>
2017-08-10 12:04:15 +0530

    display: Disable EGL buffer_age extension support
    * Due to commit I9b346b4053ec12c8a78a143a4dc0e708c44888a2
        "Support EGL_KHR_partial_update without EGL_EXT_buffer_age"
        in libhwui, EGL_EXT_buffer_age extension was forcibly enabled
        if EGL_KHR_partial_update was available, even if the EGL driver
        was not providing EGL_EXT_buffer_age support
    
     * This wrongly allows the renderthread sources to use bufferAge
        specific swap behaviours and would result in partially missing
        display redraws upon animations and rotated screens,
        for instance flashing action bars, blinking loading bars
        and half black displays in landscape rotations
    
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Signed-off-by: sub77 <sub77@ymail.com>

--- falcon.mk
+++ falcon.mk
@@ -70,6 +70,7 @@ PRODUCT_PACKAGES += \
 # Display
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
+PRODUCT_PROPERTY_OVERRIDES += debug.hwui.use_buffer_age=false
 
 # Gello
 PRODUCT_PACKAGES += \
