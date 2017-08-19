730b0c16ff247d3ba874e8770b8afd16c0953364
Kshitij Gupta <kshitijgm@gmail.com>
2017-08-19 18:10:33 +0530

    bacon: Import some kernel-headers
    Signed-off-by: sub77 <sub77@ymail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,6 +55,7 @@ TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_CONFIG := lineageos_falcon_defconfig
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8226
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
+PRODUCT_VENDOR_KERNEL_HEADERS :=  $(DEVICE_PATH)/kernel-headers
 
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
