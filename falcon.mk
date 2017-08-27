d181fb829cb64800d868519be6a97a7f9a884796
Kshitij Gupta <kshitijgm@gmail.com>
2017-08-27 20:18:29 +0000

    bacon: Add Boot control HIDL package
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Signed-off-by: sub77 <sub77@ymail.com>

--- falcon.mk
+++ falcon.mk
@@ -61,6 +61,10 @@ PRODUCT_PACKAGES += \
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 72080
 
+# Boot Control
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl
+
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images
