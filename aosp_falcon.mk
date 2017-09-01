778918f27feb8573fde7a2fecca905ca77f27639
Kshitij Gupta <kshitijgm@gmail.com>
2017-09-01 14:54:41 +0000

    falcon: aosp: Get the long list of APNs
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Signed-off-by: sub77 <sub77@ymail.com>

--- aosp_falcon.mk
+++ aosp_falcon.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Get the long list of APNs
+PRODUCT_COPY_FILES += device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+
 # Inherit from falcon device
 $(call inherit-product, device/motorola/falcon/falcon.mk)
 
