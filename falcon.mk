2cdc866af79e4317cbac428b1116d39afbe92d77
Kshitij Gupta <kshitijgm@gmail.com>
2017-09-02 13:55:00 +0000

    bacon: Remove Boot HIDL
    - We don't need this
    
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Signed-off-by: sub77 <sub77@ymail.com>

--- falcon.mk
+++ falcon.mk
@@ -61,10 +61,6 @@ PRODUCT_PACKAGES += \
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 72080
 
-# Boot Control
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl
-
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images
