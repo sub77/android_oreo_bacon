9cca677f515a3a259f14db9ed23693659b19f6c3
sub77 <sub77@ymail.com>
2017-10-05 12:48:40 +0000

    add hidl.mk

---
+++ hidl.mk
@@ -0,0 +1,111 @@
+# for HIDL related packages
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.soundtrigger@2.0-impl
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl
+
+# Camera configuration file. Shared by passthrough/binderized camera HAL
+PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-impl
+#PRODUCT_PACKAGES += \
+    camera.device@1.0-impl.legacy \
+    android.hardware.camera.provider@2.4-impl.legacy
+
+#PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-impl
+# Enable binderized camera HAL
+#PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-service
+#PRODUCT_PACKAGES += \
+    vendor.qti.hardware.camera.device@1.0
+
+# Display/Graphics
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service \
+    android.hardware.configstore@1.0-service
+
+#PRODUCT_PACKAGES += \
+    android.hardware.broadcastradio@1.0-impl
+
+#PRODUCT_PACKAGES += \
+    vendor.display.color@1.0-service \
+    vendor.display.color@1.0-impl
+
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
+
+PRODUCT_PACKAGES += \
+    android.hidl.manager@1.0-java
+
+#Enable AOSP KEYMASTER
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
+
+# GPS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0-impl
+
+# Netutils
+PRODUCT_PACKAGES += \
+    netutils-wrapper-1.0
+
+#Omx
+PRODUCT_PACKAGES += \
+    android.hardware.media.omx@1.0
+
+
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.0-impl
+
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-convert
+
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
+
+#Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
+# Wifi
+PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0-service
+
+#PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0-supplicant
+
+#RIL
+PRODUCT_PACKAGES += \
+    android.hardware.radio@1.0
+
+#Healthd packages
+PRODUCT_PACKAGES += \
+    android.hardware.health@1.0-impl \
+    android.hardware.health@1.0-service
