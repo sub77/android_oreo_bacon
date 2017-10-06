2eb07942b522294054c5470c4d089e75201a4401
sub77 <sub77@ymail.com>
2017-10-06 13:49:01 +0000

    kill power hal

--- falcon.mk
+++ falcon.mk
@@ -178,10 +178,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml
 
-# Power
-PRODUCT_PACKAGES += \
-    power.msm8226
-
 # Properties
 #PRODUCT_PROPERTY_OVERRIDES += \
     ro.sys.sdcardfs=false
