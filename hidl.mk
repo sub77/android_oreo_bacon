2367fa5fd2d5bef956fc5e16e13e4329e2e65b90
sub77 <sub77@ymail.com>
2017-10-13 02:50:25 +0000

    falcon: remove health service

--- hidl.mk
+++ hidl.mk
@@ -99,5 +99,4 @@ PRODUCT_PACKAGES += \
 
 #Healthd packages
 PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-impl \
-    android.hardware.health@1.0-service
+    android.hardware.health@1.0-impl
