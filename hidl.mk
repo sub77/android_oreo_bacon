4ede4eda7b97ed2de33c7ec6a2e1b112cfb00ff9
sub77 <sub77@ymail.com>
2017-10-13 02:49:58 +0000

    falcon: remove thermal service

--- hidl.mk
+++ hidl.mk
@@ -84,8 +84,7 @@ PRODUCT_PACKAGES += \
 
 #Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
+    android.hardware.thermal@1.0-impl
 
 # Wifi
 PRODUCT_PACKAGES += \
