ae834e09a6d53fa9b41f70a1d5842e1c313f7aef
sub77 <sub77@ymail.com>
2017-10-20 07:31:20 +0000

    hidl: build net 1.0

--- hidl.mk
+++ hidl.mk
@@ -53,7 +53,9 @@ PRODUCT_PACKAGES += \
 
 # Netutils
 PRODUCT_PACKAGES += \
-    netutils-wrapper-1.0
+    netutils-wrapper-1.0 \
+    android.system.net.netd@1.0 \
+    libandroid_net
 
 #Omx
 PRODUCT_PACKAGES += \
